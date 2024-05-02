cc8878b7aea8453136b38da503926318d60f58ff
Preetham <preetham.b@capillarytech.com>
2024-05-03 01:48:47 +0530

    indented

--- general/code/sorting.js
+++ general/code/sorting.js
@@ -13,48 +13,50 @@ class Sorting {
     console.log("Array values are : ", this.values.join("  "));
   }
 
-  mergeSort(values) {
-    if (values.length <= 1) {
-      return values;
-    }
-    const mid = Math.floor(values.length/2);
-    const left = this.mergeSort(values.slice(0, mid));
-    const right = this.mergeSort(values.slice(mid)); 
-    return merge(left, right);
 
-    function merge (left, right) {
-      const result = [];
-      let i = 0;
-      let j = 0;
+  mergeSorter = () => {
 
-      while(i < left.length && j < right.length) {
-        if (left[i] < right[j]) {
+    function mergeSort(values) {
+      if (values.length <= 1) {
+        return values;
+      }
+      const mid = Math.floor(values.length/2);
+      const left = mergeSort(values.slice(0, mid));
+      const right = mergeSort(values.slice(mid)); 
+      return merge(left, right);
+
+      function merge (left, right) {
+        const result = [];
+        let i = 0;
+        let j = 0;
+
+        while(i < left.length && j < right.length) {
+          if (left[i] < right[j]) {
+            result.push(left[i]);
+            i++;
+          } else {
+            result.push(right[j]);
+            j++;
+          }
+        }
+
+        while(i < left.length) {
           result.push(left[i]);
           i++;
-        } else {
+        }
+
+        while(j < right.length) {
           result.push(right[j]);
           j++;
         }
-      }
 
-      while(i < left.length) {
-        result.push(left[i]);
-        i++;
-      }
+        console.log(`MERGE result of left ${left} and right ${right} is : `,result)
 
-      while(j < right.length) {
-        result.push(right[j]);
-        j++;
+        return result;
       }
-      
-      console.log(`MERGE result of left ${left} and right ${right} is : `,result)
-      
-      return result;
     }
-  }
 
-  mergeSorter = () => {
-    this.values = this.mergeSort(this.values);
+    this.values = mergeSort(this.values);
   }
 }
 
