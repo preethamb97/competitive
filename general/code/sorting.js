64a0cefe4d679730646057f64cb17f4ec6ede8ca
Preetham <preethamb97@gmail.com>
2024-05-04 00:36:27 +0530

    added selection sort and xorBubblesort

--- general/code/sorting.js
+++ general/code/sorting.js
@@ -58,6 +58,47 @@ class Sorting {
 
     this.values = mergeSort(this.values);
   }
+
+  xorSorting = () => {
+    let result = [...this.values];
+    // swapping numnbers
+    let isSwapRequired = true;
+    while(isSwapRequired) {
+      isSwapRequired = false;
+      for(let i = 1; i < result.length - 1; i++) {
+        if (result[i - 1] > result[i]) {
+          result[i - 1] ^= result[i];
+          result[i] ^= result[i-1];
+          result[i - 1] ^= result[i];
+          isSwapRequired = true;
+        }
+      }
+    }
+
+    this.values = result;
+  }
+
+  selectionSorting = () => {
+    let result = [...this.values];
+    for(let i = 0; i < result.length - 1; i++) {
+      let leftValue = result[i];
+      let minIndex = i + 1;
+      for (let j = i + 1; j < result.length; j++) {
+        // console.log(`i: ${i}, j: ${j}, array Values are: ${result.join(' ')}, minIndex: ${minIndex}, minValue: ${result[minIndex]}`)
+        if (result[j + 1] < result[minIndex]) {
+          minIndex = j + 1;
+        }
+      }
+      // console.log(`Swapping i: ${i} : ${result[i]}, minIndex: ${minIndex}: ${result[minIndex]}`);
+      
+      let rightValue = result[minIndex];
+      if(rightValue < leftValue) {
+       result[i] = rightValue;
+       result[minIndex] = leftValue;
+      }
+    }
+    this.values = result;
+  }
 }
 
 const arrayValues = [5, 2, 7, 12, 1, 233];
@@ -68,4 +109,14 @@ sorter.mergeSorter();
 
 sorter.print();
 sorter.reset();
+sorter.print();
+sorter.reset();
+console.log("**********new***************");
+sorter.print();
+sorter.xorSorting();
+sorter.print();
+sorter.reset();
+console.log("**********new***************");
+sorter.print();
+sorter.selectionSorting();
 sorter.print();
