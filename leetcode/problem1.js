e6accf8703b07b6d52e3da4bc80bca925da89be4
Preetham <preethamb97@gmail.com>
2024-06-30 19:50:20 +0530

    optimized

--- leetcode/problem1.js
+++ leetcode/problem1.js
@@ -15,3 +15,37 @@ var twoSum = function(nums, target) {
         hashMap[nums[i]] = i+1;
     }
 };
+
+/* STD
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let hashMap = new Map();
+    for(let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (hashMap.has(complement)) {
+            return [hashMap.get(complement), i];
+        }
+        hashMap.set(nums[i], i);
+    }
+};
+
+
+/*
+
+var twoSum = function(nums, target) {
+    let hashMap = new Map();
+    for(let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (hashMap.has(complement)) {
+            return [hashMap.get(complement), i];
+        }
+        hashMap.set(nums[i], i);
+    }
+};
+
+*/
