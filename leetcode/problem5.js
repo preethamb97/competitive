af2cd3353385d54d0a782a167dfe7cd4c5ec8f08
Preetham <preethamb97@gmail.com>
2024-07-09 20:50:46 +0530

    optimized

--- leetcode/problem5.js
+++ leetcode/problem5.js
@@ -19,3 +19,16 @@ var removeDuplicates = function (nums) {
     }
     return j;
 };
+
+// var removeDuplicates = function (nums) {
+//     if (nums.length === 0) return 0;
+    
+//     let j = 1;
+//     for (let i = 1; i < nums.length; i++) {
+//         if (nums[i] !== nums[i - 1]) {
+//             nums[j] = nums[i];
+//             j++;
+//         }
+//     }
+//     return j;
+// };
