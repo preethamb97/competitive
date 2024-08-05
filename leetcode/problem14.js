6ac2cc654d3c64d770eca6180a4befbc6132e697
Preetham <preethamb97@gmail.com>
2024-08-05 22:09:44 +0530

    evaluate-reverse-polish-notation
    evaluate-reverse-polish-notation

---
+++ leetcode/problem14.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/?envType=study-plan-v2&envId=top-interview-150 evaluate-reverse-polish-notation
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+  const arithmetic = (op, a, b) => {
+    switch (op) {
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+      case "*":
+        return a * b;
+      case "/":
+        return a / b > 0 ? Math.floor(a / b) : Math.ceil(a / b);
+    }
+  };
+  const stack = [];
+  tokens.forEach((token) => {
+    if (["+", "-", "*", "/"].includes(token)) {
+      let res = stack.pop();
+      const popped = stack.pop();
+      res = arithmetic(token, Number(popped), Number(res));
+      stack.push(res);
+    } else {
+      stack.push(token);
+    }
+  });
+  return stack;
+};
